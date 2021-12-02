f45dfebd77d578cb6b41726c8034887f29db8f88
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-12-02 10:53:16 +0000

    Add ExploreScreen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class Fooderlich extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.dark();
+    final theme = FooderlichTheme.light();
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
